63b4b9b308f20b1b1c6c59b9a50a04430c392a4c
TrungNg <>
2018-07-06 21:05:22 +1200

    tournament selection avoid same parent; xcs_prediction not use timestamp and numerosity (same with iqbal)

--- xcs_prediction.py
+++ xcs_prediction.py
@@ -23,39 +23,19 @@ class Prediction:
         self.decision = None
         self.prediction = {}
         denominator = {}
-        self.tiebreak_numerosity = {}
-        self.tiebreak_timestamp = {}
 
         for action in cons.env.format_data.action_list:
             self.prediction[ action ] = 0.0
             denominator[ action ] = 0.0
-            self.tiebreak_numerosity[ action ] = 0
-            self.tiebreak_timestamp[ action ] = 0
 
         for ref in population.match_set:
             cl = population.pop_set[ref]
-            self.prediction[cl.action] += cl.prediction * cl.fitness# * cl.numerosity
-            denominator[cl.action] += cl.fitness# * cl.numerosity
-            self.tiebreak_numerosity[cl.action] += cl.numerosity
-            self.tiebreak_timestamp[cl.action] += cl.init_timestamp
+            self.prediction[cl.action] += cl.prediction * cl.fitness
+            denominator[cl.action] += cl.fitness
 
         for action in cons.env.format_data.action_list:
             if denominator[ action ] != 0:
                 self.prediction[ action ] /= denominator[ action ]
-            #else:
-            #    self.prediction[eachClass] = 0
-
-        max_prediction = 0.0
-        self.best_set = [] #Prediction is set up to handle best class ties for problems with more than 2 classes
-        for action in cons.env.format_data.action_list:
-            if self.tiebreak_numerosity[ action ] != 0 and self.prediction[ action ] >= max_prediction:
-                max_prediction = self.prediction[ action ]
-
-        for action in cons.env.format_data.action_list:
-            if self.tiebreak_numerosity[ action ] != 0 and self.prediction[ action ] == max_prediction: #Tie for best class
-                self.best_set.append( action )
-        self.possible_actions = [ k for k,_ in self.prediction.items() if self.tiebreak_numerosity[k] != 0 ]
-        self.possible_actions.sort()
 
 
     def getFitnessSum(self,population,low,high):
@@ -76,34 +56,10 @@ class Prediction:
     def decide(self, explore=True):
         """ Returns prediction decision. """
         if explore:
-            self.decision = random.choice( self.possible_actions )
-        elif len( self.best_set ) == 1:
-            self.decision = self.best_set[0]
+            self.decision = random.choice( cons.env.format_data.action_list )
         else:
-            max_numerosity = 0
-            new_best_action = []
-            for action in self.best_set:
-                if self.tiebreak_numerosity[action] >= max_numerosity:
-                    max_numerosity = self.tiebreak_numerosity[action]
-            for action in self.best_set:
-                if self.tiebreak_numerosity[action] == max_numerosity:
-                    new_best_action.append(action)
-            # -----------------------------------------------------------------------
-            if len(new_best_action) > 1:  # still a tie
-                latest_created = 0
-                newest_best_action = []
-                for action in new_best_action:
-                    if self.tiebreak_timestamp[action] >= latest_created:
-                        latest_created = self.tiebreak_timestamp[action]
-
-                for action in new_best_action:
-                    if self.tiebreak_timestamp[action] == latest_created:
-                        newest_best_action.append(action)
-                # -----------------------------------------------------------------------
-                if len( newest_best_action ) == 1:  # Prediction is completely tied - XCS has no useful information for making a prediction
-                    self.decision = newest_best_action[0]
-                else:
-                    self.decision = random.choice( newest_best_action )
-            else:
-                self.decision = new_best_action[0]
+            self.decision = cons.env.format_data.action_list[0]
+            for action in cons.env.format_data.action_list:
+                if self.prediction[ action ] > self.prediction[ self.decision ]:
+                    self.decision = action
         return self.decision
