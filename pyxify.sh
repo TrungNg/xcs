fae5f4f12279d9c8bf06d39a630d641b21e039c5
trungnb <trungnbao@gmail.com>
2018-07-10 03:34:54 +1200

    ...

--- pyxify.sh
+++ pyxify.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+if [ ! -d ./cysrc ]; then
+    mkdir cysrc
+fi
+
 for file in ./xcs_*.py
 do
-        echo "changing extension and moving $file"
-        cp ${file##*/} ./cysrc/${file##*/}x
-        echo "done ${file##*/}"
+    echo "changing extension and moving $file"
+    cp ${file##*/} ./cysrc/${file##*/}x
+    echo "done ${file##*/}"
 done
