843ad2987fb01736c37728c71a1f79e9836d379c
TrungNg <>
2018-10-01 19:24:50 +1300

    refine data display

--- xcs_classifier.py
+++ xcs_classifier.py
@@ -469,13 +469,13 @@ class Classifier:
         specificity = len(self.condition) / float(cons.env.format_data.numb_attributes)
 
         if cons.env.format_data.discrete_action:
-            classifier_info += str(self.action)+"\t"
+            classifier_info += '{0:>5}'.format(self.action)+"\t"
         else:
-            classifier_info += str(self.action[0])+';'+str(self.action[1])+"\t"
+            classifier_info += '{0:>3}'.format(self.action[0])+';'+'{0:>3}'.format(self.action[1])+"\t"
         #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        classifier_info += '{:.1f}'.format(self.prediction)+"\t"+'{:.2f}'.format(self.error)+"\t"+'{:.2f}'.format(self.fitness)+"\t"+str(self.numerosity)+"\t"+str(self.ga_count)+"\t"
-        classifier_info += '{:.1f}'.format(self.avg_actionset_size)+"\t\t"+str(self.ga_timestamp)+"\t\t"+str(self.init_timestamp)+"\t\t"+'{:.2f}'.format(specificity)+"\t\t"
-        classifier_info += '{:.1f}'.format(self.delete_vote)+"\t\t"+str(self.action_cnt)+"\n"
+        classifier_info += '{0:>6.1f}'.format(self.prediction)+'\t'+'{0:>6.1f}'.format(self.error)+'\t'+'{0:>5.3f}'.format(self.fitness)+"\t"+'{0:>4}'.format(self.numerosity)+'\t'+'{0:>5}'.format(self.ga_count)+'\t'
+        classifier_info += '{0:>6.1f}'.format(self.avg_actionset_size)+'\t'+'{0:>8}'.format(self.ga_timestamp)+'\t'+'{0:>8}'.format(self.init_timestamp)+'\t'+'{:.2f}'.format(specificity)+'\t'
+        classifier_info += '{:>10.1f}'.format(self.delete_vote)+'\t'+'{:>8}'.format(self.action_cnt)+'\n'
 
         #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         return classifier_info
