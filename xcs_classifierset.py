3c073ddc9399a20d93e5a94920316b13979abdf4
trungnb <trungnbao@gmail.com>
2018-10-02 02:48:37 +1300

    ...

--- xcs_classifierset.py
+++ xcs_classifierset.py
@@ -585,7 +585,7 @@ class ClassifierSet:
         while i < len(self.pop_set):
             cl = self.pop_set[i]
             if cl.action_cnt <= 100 or cl.error >= 0.0001:
-                self.removeMacroClassifier(i)
+                self.pop_set.pop(i)
                 self.micro_size -= 1
             else:
                 i += 1
